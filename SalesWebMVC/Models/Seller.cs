67fe11b717578c756c0c90c3116e071b3902dc37
jrd-learn <105658679+jrd-learn@users.noreply.github.com>
2023-02-09 22:01:02 -0300

    Added models (Department, Seller, SalesRecord). Added Enum (SaleStatus) Moved ErrorViewModel to new folder ViewModels Fixed HomeController and View/Error after moved ErrorViewModel

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,42 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            SalesRecords.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            SalesRecords.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
