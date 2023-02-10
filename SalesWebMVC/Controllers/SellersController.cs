56fc1de1ebb3eda0560d4fa0bfe638336f02c7b7
jrd-learn <105658679+jrd-learn@users.noreply.github.com>
2023-02-10 16:23:37 -0300

    Implemented Sellers View with new ViewModel

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,12 +1,145 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
-        public IActionResult Index()
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
+            _sellerService = sellerService;
+            _departmentService = departmentService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var sellers = await _sellerService.ListAllAsync();
+            return View(sellers);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var seller = await _sellerService.GetByIdAsync(id);
+
+            if (seller == null)
+            {
+                return RedirectToAction(nameof(Index), new { message = "ID not provided." });
+            }
+
+            return View(seller);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var seller = await _sellerService.GetByIdAsync(id);
+
+            if (seller is null)
+            {
+                return RedirectToAction(nameof(Index), new { message = "ID not found." });
+            }
+
+            var departments = await _departmentService.ListAllAsync();
+
+            SellerFormViewModel viewModel = new() { Seller = seller, Departments = departments };
+
+            ViewData["DepartmentId"] = new SelectList(departments, "Id", "Name", seller.DepartmentId);
+
+            return View(viewModel);
+
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var seller = await _sellerService.GetByIdAsync(id);
+
+            if (seller == null)
+            {
+                return RedirectToAction(nameof(Index), new { message = "ID not provided." });
+            }
+
+            return View(seller);
+        }
+        public async Task<IActionResult> Create()
+        {
+            var departments = await _departmentService.ListAllAsync();
+            
+            ViewData["DepartmentId"] = new SelectList(departments, "Id", "Name");
+            
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _sellerService.UpdateAsync(seller);
+                    return RedirectToAction(nameof(Details), id);
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    return RedirectToAction(nameof(Index), new { message = $"Error: {e.Message}" });
+                }
+            }
+
+            return RedirectToAction(nameof(Edit), id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return RedirectToAction(nameof(Index), new { message = "ID not provided." });
+            }
+
+            try
+            {
+                await _sellerService.DeleteAsync(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return RedirectToAction(nameof(Index), new { message = $"Error: {e.Message}" });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _sellerService.UpdateAsync(seller);
+                    return RedirectToAction(nameof(Details), id);
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    return RedirectToAction(nameof(Index), new { message = $"Error: {e.Message}" });
+                }
+            }
+
+            return RedirectToAction(nameof(Edit), id);
         }
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,6 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
@@ -11,6 +13,29 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-
+        public async Task<List<Seller>> ListAllAsync()
+        {
+            return await _context.Sellers.Include(x => x.Department).ToListAsync();
+        }
+        public async Task<Seller> GetByIdAsync(int id)
+        {
+            return await _context.Sellers.Include(x => x.Department).FirstOrDefaultAsync(x => x.Id == id);
+        }
+        
+        public async Task UpdateAsync(Seller seller)
+        {
+            _context.Update(seller);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Seller seller)
+        {
+            _context.Remove(seller);
+            await _context.SaveChangesAsync();
+        }
+        public async Task CreateAsync(Seller seller)
+        {
+            _context.Add(seller);
+            await _context.SaveChangesAsync();
+        }
     }
 }
