1842d20fc2ef9bca4aa69e7b5687bcdae69b17da
jrd-learn <105658679+jrd-learn@users.noreply.github.com>
2023-02-22 17:35:47 -0300

    Added bootstrap lux theme, updated departments and sellers views.

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -6,8 +6,6 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Display(Name="Birth Date")]
